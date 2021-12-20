21974517a01c175cb068960bfe357bdc868ef729
Travis Harris <me@travis.dev>
2021-12-20 14:15:04 -0800

    Fix

--- app/routes/blog/$post.tsx
+++ app/routes/blog/$post.tsx
@@ -1,16 +1,4 @@
-import styles from "@hydralite/vortex/dist/style.css";
-import Blog from "@hydralite/vortex";
-import { ClientOnly, useHydrated } from "remix-utils";
-
-export function links() {
-  return [{ rel: "stylesheet", href: styles }];
-}
-
 export default function BlogPost() {
-  let isHydrated = useHydrated();
 
-  if (isHydrated) {
-    return <Blog layout="Hashnode" content="Test Content" />;
-  }
   return <div>Loading</div>;
 }
