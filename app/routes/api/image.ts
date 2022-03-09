33738fb64fa5d9636c8326611a435f684c70c581
Travis Harris <travisharris@microsoft.com>
2022-03-08 23:09:07 -0800

    Fixes

--- app/routes/api/image.ts
+++ app/routes/api/image.ts
@@ -1,9 +1,14 @@
 import type { LoaderFunction } from "remix";
-import { imageLoader, DiskCache } from "remix-image/server";
+import {
+  imageLoader,
+  MemoryCache,
+  pureTransformer,
+} from "remix-image/serverPure";
 
 const config = {
   selfUrl: "http://localhost:3000",
-  cache: new DiskCache(),
+  cache: new MemoryCache(),
+  transformer: pureTransformer,
 };
 
 export const loader: LoaderFunction = ({ request }) => {
